ff8937beaea10d3b3b5dae6188a1514726b5354b
cristian reyes <cristianreyes@MacBook-Air-de-cristian.local>
2025-11-10 18:47:38 -0500

    adjust dependencies conflicts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const frontendUrl = configService.get<string>('FRONTEND_URL') || '*';
+
+  app.enableCors({
+    origin: frontendUrl, // asegúrate que sea el dominio de Vercel
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
+
 
